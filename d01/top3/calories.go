99d996c0ef9d652de446dd095fd496dc8dcb79cf
Adam Solove <asolove@gmail.com>
2022-12-01 10:27:47 -0500

    Day 1 part 2 basics

---
+++ d01/top3/calories.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func topThree(challenger, a, b, c int) (int, int, int) {
+	if challenger > c {
+		if challenger > b {
+			if challenger > a {
+				return challenger, a, b;
+			} else {
+				return a, challenger, b;
+			}
+		} else {
+			return a, b, challenger;
+		}
+	}
+	return a, b, c;
+}
+
+func main() {
+	var a, b, c int;
+	var currentTotal int;	
+
+	// Would be nice to parse at two layers: first to find the per-elf groups and then to consume the numbers inside them.
+	s := bufio.NewScanner(os.Stdin);
+	for s.Scan() {
+		if t := s.Text(); t == "" {
+			a, b, c = topThree(currentTotal, a, b, c);
+			currentTotal = 0;
+		} else {
+			if n, err := strconv.Atoi(t); err == nil {
+				currentTotal += n;
+			}
+		}
+	}
+	// Would be nice to not have to run this separately for final round without newline
+	a, b, c = topThree(currentTotal, a, b, c);
+	currentTotal = 0;
+
+	fmt.Printf("%d %d %d %d\n", a + b + c, a, b, c);
+	return;
+}
