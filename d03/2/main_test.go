c93184b13ca02c0bd45ab775b963ca0fd05ea5e5
Adam Solove <asolove@gmail.com>
2022-12-03 06:58:08 -0500

    Day 3, part 2

---
+++ d03/2/main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import "testing"
+
+func TestBadgeItem(t *testing.T) {
+  expected := 'Z';
+  r1 := "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn";
+  r2 := "ttgJtRGJQctTZtZT";
+  r3 := "CrZsJsPPZsGzwwsLwLmpwMDw";
+  actual := badgeItem(r1, r2, r3);
+
+  if actual != expected {
+    t.Errorf("Expected %c but got %c", expected, actual);
+  }
+}
+
+func TestItemPriority(t *testing.T) {
+  table := map[rune]int {
+    'a': 1,
+    'z': 26,
+    'A': 27,
+    'Z': 52,
+  };
+
+  for item, expected := range table {
+    if actual := itemPriority(item); actual != expected {
+      t.Errorf("ItemPriority(%c): got %d, expected %d", item, actual, expected);
+    }
+  }
+}
