c379f0350b819fe7dc0c9f10abd2ae2bf411abac
Adam Solove <asolove@gmail.com>
2022-12-04 16:03:51 -0500

    Trying out go debugger.
    Use arg rather than stdin pipe: that's what VSCode launcher can handle?

--- d01/top3/calories.go
+++ d01/top3/calories.go
@@ -26,8 +26,12 @@ func main() {
 	var a, b, c int
 	var currentTotal int
 
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		panic("Can't open data file")
+	}
 	// Would be nice to parse at two layers: first to find the per-elf groups and then to consume the numbers inside them.
-	s := bufio.NewScanner(os.Stdin)
+	s := bufio.NewScanner(f)
 	for s.Scan() {
 		if t := s.Text(); t == "" {
 			a, b, c = topThree(currentTotal, a, b, c)
@@ -38,9 +42,9 @@ func main() {
 			}
 		}
 	}
+
 	// Would be nice to not have to run this separately for final round without newline
 	a, b, c = topThree(currentTotal, a, b, c)
 
-	fmt.Printf("%d\n", a+b+c)
-	return
+	fmt.Printf("Total of top 3: %d\n", a+b+c)
 }
