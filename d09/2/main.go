529fd4bb7e05fece0b6e04d1d65aa189418dd2c6
Adam Solove <asolove@gmail.com>
2022-12-09 07:23:40 -0500

    Day 9, part 2

---
+++ d09/2/main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	moves := parseMoves()
+
+	states := make([]State, 9)
+	tailSpots := make(map[string]bool)
+	for _, move := range moves {
+		for i := 0; i < move.steps; i++ {
+			var prevState State
+			for j, state := range states {
+				if j == 0 {
+					state = stepHead(state, move.dir)
+				} else {
+					state.headX = prevState.tailX
+					state.headY = prevState.tailY
+				}
+
+				state = stepTail(state)
+
+				if j == 8 {
+					tailSpots[fmt.Sprintf("%d,%d", state.tailX, state.tailY)] = true
+				}
+				states[j] = state
+				prevState = state
+			}
+		}
+		// fmt.Printf("After move (%v):\n  %v\n", move, states[8])
+	}
+	fmt.Printf("Visited by tail: %d\n", len(tailSpots))
+}
+
+func stepHead(state State, dir Dir) State {
+	switch dir {
+	case Up:
+		state.headY -= 1
+	case Down:
+		state.headY += 1
+	case Left:
+		state.headX -= 1
+	case Right:
+		state.headX += 1
+	}
+	return state
+}
+
+func stepTail(state State) State {
+	xd := state.headX - state.tailX
+	xm := xd < -1 || xd > 1
+	yd := state.headY - state.tailY
+	ym := yd < -1 || yd > 1
+
+	if !xm && !ym {
+		return state
+	}
+
+	if xd >= 1 {
+		state.tailX += 1
+	}
+	if xd <= -1 {
+		state.tailX -= 1
+	}
+	if yd >= 1 {
+		state.tailY += 1
+	}
+	if yd <= -1 {
+		state.tailY -= 1
+	}
+	return state
+}
+
+type State struct {
+	headX int
+	headY int
+	tailX int
+	tailY int
+}
+
+type Dir string
+
+const (
+	Up    Dir = "U"
+	Down      = "D"
+	Left      = "L"
+	Right     = "R"
+)
+
+type Move struct {
+	dir   Dir
+	steps int
+}
+
+func parseMoves() []Move {
+	moves := make([]Move, 0)
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		words := strings.Split(s.Text(), " ")
+		moves = append(moves, Move{dir: atod(words[0]), steps: atoi(words[1])})
+	}
+	return moves
+}
+
+func atod(s string) Dir {
+	dirs := map[string]Dir{"U": Up, "D": Down, "L": Left, "R": Right}
+	return dirs[s]
+}
+
+func atoi(i string) int {
+	n, e := strconv.Atoi(i)
+	if e != nil {
+		panic("Can't parse to digit")
+	}
+	return n
+}
